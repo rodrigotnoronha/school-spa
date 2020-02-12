dbcb134080fa717ac40a1ad22a248b93ea93764d
rodrigotnoronha <rodrigotnoronha@gmail.com>
2020-02-11 23:52:09 -0300

    modified:   config/prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@ var merge = require('webpack-merge')
 var common = require('./import-common')
 
 module.exports = merge({
-  API_URL: '"http://localhost:8000/"',
+  API_URL: '"http://escola-app.herokuapp.com/"',
   NODE_ENV: '"production"',
   DEBUG: false
 }, common)
